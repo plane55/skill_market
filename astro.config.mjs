214df2d0df50dd0873c7b406ea38fbad54cd23b7
plane55 <1239273491@qq.com>
2026-05-23 09:16:13 +0800

    page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import vue from '@astrojs/vue'
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), vue()],
   output: 'static',
+  site: 'https://plane55.github.io',
+  base: '/skill_market',
 })
